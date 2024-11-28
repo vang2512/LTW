3a1b8410dffc2996871319941816a2d042ef406b
Anh-Tu2912 <22130307@st.hcmuaf.edu.vn>
2024-11-29 00:19:08 +0700

    Xử lí chuyển đổi giữa các tính năng trang admin

--- Admin/scripts.js
+++ Admin/scripts.js
@@ -1,3 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('.sidebar a');
+    const sections = document.querySelectorAll('.content-section');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            const contentId = this.getAttribute('data-target');
+
+            links.forEach(link => link.classList.remove('active'));
+
+            this.classList.add('active');
+
+            sections.forEach(section => {
+                section.classList.remove('active');
+                section.style.display = 'none';
+            });
+
+            const activeSection = document.getElementById(contentId);
+
+            if (JSON.stringify(contentId) === JSON.stringify('analyticsContent')) {
+                activeSection.style.display = 'grid';
+            } else {
+                activeSection.style.display = 'flex';
+            }
+            setTimeout(() => {
+                activeSection.classList.add('active');
+            }, 10); // Delay to trigger CSS transition
+        });
+    });
+
+    // Hiển thị nội dung mặc định
+    const defaultSection = document.getElementById('analyticsContent');
+    defaultSection.style.display = 'grid';
+    setTimeout(() => {
+        defaultSection.classList.add('active');
+    }, 10);
+});
+
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
