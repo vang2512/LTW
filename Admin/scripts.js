9cdec905a920f1bc0483c5cf544bfd6665c363f0
Anh-Tu2912 <22130307@st.hcmuaf.edu.vn>
2024-11-29 13:34:32 +0700

    Tạo phương thức xử lí dữ liệu trang admin

--- Admin/scripts.js
+++ Admin/scripts.js
@@ -37,15 +37,27 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 10);
 });
 
+users.forEach(user => {
+    const tr = document.createElement('tr');
+    const trContent = `
+            <td>${user.id}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>${user.phone}</td>
+    `;
+    tr.innerHTML = trContent;
+    document.querySelector('.users-table table tbody').appendChild(tr);
+});
+
 Orders.forEach(order => {
     const tr = document.createElement('tr');
     const trContent = `
         <td>${order.productName}</td>
         <td>${order.productNumber}</td>
         <td>${order.paymentStatus}</td>
-        <td class="${order.status === 'Pending' ? 'danger' : order.status === 'Deposit' ? 'warning' : 'primary'}">${order.status}</td>
+        <td class="${order.status === 'Declined' ? 'danger' : order.status === 'Pending' ? 'warning' : 'primary'}">${order.status}</td>
         <td class="primary">Details</td>
     `;
     tr.innerHTML = trContent;
-    document.querySelector('table tbody').appendChild(tr);
+    document.querySelector('.recent-orders table tbody').appendChild(tr);
 });
