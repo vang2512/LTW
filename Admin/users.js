d20ff3a857a1071f53aa11e27f1f8dde3ac09c59
Anh-Tu2912 <22130307@st.hcmuaf.edu.vn>
2024-12-01 16:36:28 +0700

    Thêm dữ liệu người dùng trang admin

--- Admin/users.js
+++ Admin/users.js
@@ -1,20 +1,50 @@
 const users = [
     {
-        id: 1,
+        number: 1,
+        id: 'KH01',
         name: 'Nguyễn Văn A',
         email: 'nguyenvana@gmail.com',
-        phone: '0123456789'
+        address: '731 Trần Hưng Đạo, Q5, TpHCM',
+        phone: '08823451'
     },
     {
-        id: 2,
-        name: 'Trần Thị B',
+        number: 2,
+        id: 'KH02',
+        name: 'Trần Ngọc Hân',
         email: 'tranthib@outlook.com',
-        phone: '0135792468'
+        address: '23/5 Nguyễn Trãi, Q5, TpHCM',
+        phone: '0908256478'
     },
     {
-        id: 3,
-        name: 'Lê Văn C',
+        number: 3,
+        id: 'KH03',
+        name: 'Hà Duy Lập',
         email: 'levanc@yahoo.com',
-        phone: '0928173645'
+        address: '34/34B Nguyễn Trãi, Q1, TpHCM',
+        phone: '08768904'
+    },
+    {
+        number: 4,
+        id: 'KH04',
+        name: 'Trần Ngọc Linh',
+        email: 'ngoclinhtran@gmail.com',
+        address: '45 Nguyễn Canh Chàn, Q1, TpHCM',
+        phone: '0938776266'
+    },
+    {
+        number: 5,
+        id: 'KH05',
+        name: 'Trần Minh Long',
+        email: 'longtran213c@outlook.com',
+        address: '50/34 Le Dai Hanh, Q10, TpHCM',
+        phone: '0917325476'
+    },
+    {
+        number: 6,
+        id: 'KH06',
+        name: 'Lê Nhật Minh',
+        email: 'minhle1241@gmail.com',
+        address: '34 Trường Chinh, Q3, TpHCM',
+        phone: '08246108'
     },
 ]
