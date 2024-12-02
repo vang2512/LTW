d4a583edb470e713d2aa0f2368c53f9ff5e4f647
Anh-Tu2912 <22130307@st.hcmuaf.edu.vn>
2024-12-03 00:13:13 +0700

    Khởi tạo sơ bộ dữ liệu sản phẩm chức năng quản lí sản phẩm trang admin

---
+++ Admin/products.js
@@ -0,0 +1,30 @@
+const products = [
+    {
+        number: 1,
+        id: 'Coffe#01',
+        name: 'Cappuccino',
+        type: 'Coffee',
+        price: 30000
+    },
+    {
+        number: 2,
+        id: 'Cake#01',
+        name: 'Tiramisu',
+        type: 'Cake',
+        price: 50000
+    },
+    {
+        number: 3,
+        id: 'Coffee#02',
+        name: 'Bạc xỉu',
+        type: 'Coffee',
+        price: 20000
+    },
+    {
+        number: 4,
+        id: 'Food#01',
+        name: 'Bánh mì que',
+        type: 'Food',
+        price: 12000
+    },
+]
