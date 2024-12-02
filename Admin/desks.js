49eff62126fa0defc3dca650ec3850d8385da9bc
Anh-Tu2912 <22130307@st.hcmuaf.edu.vn>
2024-12-03 00:22:52 +0700

    Khởi tạo sơ bộ dữ liệu bàn chức năng quản lí bàn trang admin

---
+++ Admin/desks.js
@@ -0,0 +1,51 @@
+const desks = [
+    {
+        number: 1,
+        type: 'Bàn 2 người',
+        space: 'Ngoài trời',
+        amount: 20,
+        price: 50000
+    },
+    {
+        number: 2,
+        type: 'Bàn 2 người',
+        space: 'Trong quán',
+        amount: 10,
+        price: 100000
+    },
+    {
+        number: 4,
+        type: 'Bàn 4 người',
+        space: 'Ngoài trời',
+        amount: 10,
+        price: 100000
+    },
+    {
+        number: 5,
+        type: 'Bàn 4 người',
+        space: 'Trong quán',
+        amount: 8,
+        price: 200000
+    },
+    {
+        number: 7,
+        type: 'Bàn 6 người',
+        space: 'Ngoài trời',
+        amount: 5,
+        price: 150000
+    },
+    {
+        number: 8,
+        type: 'Bàn 6 người',
+        space: 'Trong quán',
+        amount: 2,
+        price: 300000
+    },
+    {
+        number: 9,
+        type: 'Bàn 6 người',
+        space: 'Phòng riêng',
+        amount: 2,
+        price: 1000000
+    },
+]
