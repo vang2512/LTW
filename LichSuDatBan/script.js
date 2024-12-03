00baae36194cab4058e4c15f0b04eef6f0cbe2f1
nguyen van vang <22130321@st.hcmuaf.edu.vn>
2024-12-02 18:32:43 -0800

    cập nhật header & footer phần trang không gian quán; tạo trang lịch sử đặt bàn

---
+++ LichSuDatBan/script.js
@@ -0,0 +1,29 @@
+
+<!--JS Header & Footer-->
+document.addEventListener('DOMContentLoaded', () => {
+    const cache = new Map();
+    async function loadContent() {
+        const elements = document.querySelectorAll('[data-include]');
+        for (const el of elements) {
+            const file = el.getAttribute('data-include');
+            if (file) {
+                if (cache.has(file)) {
+                    el.innerHTML = cache.get(file);
+                } else {
+                    const response = await fetch(file);
+                    if (response.ok) {
+                        const text = await response.text();
+                        cache.set(file, text);
+                        el.innerHTML = text;
+                    } else {
+                        el.innerHTML = "Không thể tải nội dung.";
+                    }
+                }
+            }
+        }
+    }
+    loadContent();
+});
+
+
+        
