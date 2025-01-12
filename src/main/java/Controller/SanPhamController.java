5d41a95f4c0bfe730cfaa126800da98fcabe07b3
Phamvudz123 <22130330@st.hcmuaf.edu.vn>
2025-01-12 22:31:22 +0700

    sửa lại chút sản phẩm controller

---
+++ src/main/java/Controller/SanPhamController.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import Dao.SanPhamDao;
+import Model.SanPham;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+public class SanPhamController extends HttpServlet {
+    private SanPhamDao sanPhamDao = new SanPhamDao();
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<SanPham> cafeList = sanPhamDao.getSanPhamByLoai(1);
+        List<SanPham> nuocEpList = sanPhamDao.getSanPhamByLoai(2);
+        List<SanPham> traList = sanPhamDao.getSanPhamByLoai(3);
+        List<SanPham> banhList = sanPhamDao.getSanPhamByLoai(4);
+        request.setAttribute("cafeList", cafeList);
+        request.setAttribute("nuocEpList", nuocEpList);
+        request.setAttribute("traList", traList);
+        request.setAttribute("banhList", banhList);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("pages/Mat_hang.jsp");
+        dispatcher.forward(request, response);
+    }
+}
